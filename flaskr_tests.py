043dad6350320c6af01887b73180785545f632f1
Your Name <you@example.com>
2015-03-25 16:30:34 +1100

    Update on Users with entires

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -76,21 +76,37 @@ class FlaskrTestCase(unittest.TestCase):
         assert '&lt;Hello&gt;' in rv.data
         assert '<strong>HTML</strong> allowed here' in rv.data
 
-	def User_2(self):
-		self.login('jim', 'bean')
+	def User_1(self):
+		self.login('admin', 'default')
         rv = self.app.post('/add', data=dict(
             title='<Hello>',
             text='<strong>HTML</strong> allowed here',
-			start_time= '<14:00>',
-			end_time= '<16:30>'
+			start_time= '<15:00>',
+			end_time= '<17:30>'
         ), follow_redirects=True)
         assert 'No entries here so far' not in rv.data	
-        assert '&lt;Doneon09March2015&gt;' in rv.data
         assert '&lt;Hello&gt;' in rv.data
         assert '<strong>HTML</strong> allowed here' in rv.data
-        assert 'by jim' in rv.data
+        assert 'by admin' in rv.data
+        assert '&lt;15:00&gt;' in rv.data
+        assert '&lt;17:30&gt;' in rv.data
 		
-				
+	def User_2(self):
+		self.login('jim', 'bean')
+        rv = self.app.post('/add', data=dict(
+            title='<C++_Exercise_01>',
+            text='<strong>HTML</strong> allowed here',
+			start_time= '<15:00>',
+			end_time= '<16:00>'
+        ), follow_redirects=True)
+        assert 'No entries here so far' not in rv.data	
+        assert '&lt;C++_Exercise_01&gt;' in rv.data
+        assert '<strong>HTML</strong> allowed here' in rv.data
+        assert 'by admin' in rv.data
+        assert '&lt;15:00&gt;' in rv.data
+        assert '&lt;16:00&gt;' in rv.data
+
+		
 	def test_time(self):
 		self.login('admin', 'default')
         rv = self.app.post('/add', data=dict(
